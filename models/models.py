6c5504bffa74975cbc6bb6d8e9271c297fac6642
Paszo <paszo2008@gmail.com>
2019-01-19 22:01:13 +0100

    [ADD] Add the model corrections and security

--- models/models.py
+++ models/models.py
@@ -2,12 +2,13 @@
 
 from odoo import models, fields, api
 
+
 class CarRequest(models.Model):
     _name = "car.request" # Table in DB => car_request
     _description = "Car Request"
 
     name = fields.Char(string="Request", required=True, )
-    date_form = fields.Datetime(string="Starting Date", default=fields.Datetime.now(), )
+    date_from = fields.Datetime(string="Starting Date", default=fields.Datetime.now(), )
     date_to = fields.Datetime(string="End Date", required=False, )
     employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", required=True, )
-    car_id = field.Many2one(comodel_name="fleet.vehicle", string="Car", required=True )
+    car_id = fields.Many2one(comodel_name="fleet.vehicle", string="Car", required=True, )
